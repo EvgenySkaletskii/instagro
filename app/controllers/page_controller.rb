864543b1f59f304a977fd03ae1a2e77b1339ea07
Evgeny Skaletskii <evgeny.skaletskii@jetruby.com>
2022-01-30 18:48:30 +0300

    Add show, edit, and update methods for user

--- app/controllers/page_controller.rb
+++ app/controllers/page_controller.rb
@@ -1,4 +1,9 @@
 class PageController < ApplicationController
   def index
+    if signed_in?
+      redirect_to user_path(current_user)
+    else
+      redirect_to new_user_session_path
+    end
   end
 end
