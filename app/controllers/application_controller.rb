0d664db6772c1eda9aeef2ec62ca2e0af6dab1e6
Evgeny Skaletskii <evgeny.skaletskii@jetruby.com>
2022-01-31 09:52:52 +0300

    Add welcome page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+  rescue_from ActionController::RoutingError, with: :route_not_found
+
+  private
+
+  def record_not_found
+    render plain: "Page not found"
+  end
 end
--- app/controllers/page_controller.rb
+++ app/controllers/page_controller.rb
@@ -2,8 +2,6 @@ class PageController < ApplicationController
   def index
     if signed_in?
       redirect_to user_path(current_user)
-    else
-      redirect_to new_user_session_path
     end
   end
 end
