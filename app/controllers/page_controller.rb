66847513405c555153c3b5cce968cf323a671ded
Evgeny Skaletskii <evgeny.skaletskii@jetruby.com>
2022-02-12 00:42:49 +0300

    add home page

--- app/controllers/page_controller.rb
+++ app/controllers/page_controller.rb
@@ -1,7 +1,11 @@
 class PageController < ApplicationController
-  def index
-    if signed_in?
-      redirect_to user_path(current_user)
-    end
+  # def index
+  #   if signed_in?
+  #     redirect_to home_path
+  #   end
+  # end
+
+  def home
+    @post = current_user.posts.build if user_signed_in?
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users, :controllers => { registrations: "users/registrations" }
-  root "page#index"
+  get "/" => redirect("/home")
+  get "/home", to: "page#home"
   resources :users, only: [:show, :edit, :update]
   resources :posts, only: [:create, :destroy]
 end
