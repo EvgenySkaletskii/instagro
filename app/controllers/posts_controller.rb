7e18b18b2e75a6d271e4a4a1b699b8ac27d0888f
Evgeny Skaletskii <evgeny.skaletskii@jetruby.com>
2022-02-13 12:43:31 +0300

    add pundit and posts destroy authorization

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,5 @@
 class PostsController < ApplicationController
   before_action :authenticate_user!, only: [:create, :destroy]
-  before_action :correct_user, only: [:destroy]
 
   def create
     @post = current_user.posts.build(post_params)
@@ -13,6 +12,8 @@ class PostsController < ApplicationController
   end
 
   def destroy
+    @post = Post.find(params[:id])
+    authorize @post
     @post.destroy
     flash[:notice] = "Post has been successfully deleted!"
     redirect_to request.referrer || feed_path
@@ -22,10 +23,5 @@ class PostsController < ApplicationController
 
   def post_params
     params.require(:post).permit(:content)
-  end
-
-  def correct_user
-    @post = current_user.posts.find_by(id: params[:id])
-    redirect_to root_url if @post.nil?
   end
 end
---
+++ app/policies/post_policy.rb
@@ -0,0 +1,12 @@
+class PostPolicy
+  attr_reader :user, :post
+
+  def initialize(user, post)
+    @user = user
+    @post = post
+  end
+
+  def destroy?
+    user.admin? || user.posts.find(@post.id)
+  end
+end
