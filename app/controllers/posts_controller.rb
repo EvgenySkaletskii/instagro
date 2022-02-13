12f9731d62b7b16f6f40a2b368b41b50de12c5fc
Evgeny Skaletskii <evgeny.skaletskii@jetruby.com>
2022-02-13 11:17:44 +0300

    add delete button for posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,22 +1,31 @@
 class PostsController < ApplicationController
   before_action :authenticate_user!, only: [:create, :destroy]
+  before_action :correct_user, only: [:destroy]
 
   def create
     @post = current_user.posts.build(post_params)
     if @post.save
       flash[:notice] = "Post has been successfully created!"
     else
-      flash[:alert] = "Post wasn't saved. The description s empty."
+      flash[:alert] = "Post wasn't saved. The description is empty."
     end
-    redirect_to current_user
+    redirect_to feed_path
   end
 
   def destroy
+    @post.destroy
+    flash[:notice] = "Post has been successfully deleted!"
+    redirect_to request.referrer || feed_path
   end
 
   private
 
   def post_params
     params.require(:post).permit(:content)
+  end
+
+  def correct_user
+    @post = current_user.posts.find_by(id: params[:id])
+    redirect_to root_url if @post.nil?
   end
 end
