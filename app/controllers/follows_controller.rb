88f87fdfcf8c26e62786b7cc9da5c2854e525b5d
Evgeny Skaletskii <evgeny.skaletskii@jetruby.com>
2022-02-18 18:33:31 +0300

    add follow-unfollow buttons to user partial

--- app/controllers/follows_controller.rb
+++ app/controllers/follows_controller.rb
@@ -2,12 +2,12 @@ class FollowsController < ApplicationController
   def create
     user = User.find(params[:followed_id])
     current_user.follow(user)
-    redirect_to user
+    redirect_to request.referrer || user
   end
 
   def destroy
     user = Follow.find(params[:id]).followed
     current_user.unfollow(user)
-    redirect_to user
+    redirect_to request.referrer || user
   end
 end
