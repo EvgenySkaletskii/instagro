e021612bcb89f41cffd5050f3327860f3168b820
Evgeny Skaletskii <evgeny.skaletskii@jetruby.com>
2022-02-18 15:34:03 +0300

    update tests for user profiles with follows

--- spec/features/user_profile_spec.rb
+++ spec/features/user_profile_spec.rb
@@ -5,18 +5,54 @@ RSpec.describe "User profile page", type: :feature do
     @user = create :user
     @post1 = create(:post, content: "Post#1", created_at: 10.minutes.ago, user: @user)
     @post2 = create(:post, content: "Post#2", created_at: 5.minutes.ago, user: @user)
+    @anton = create(:user, full_name: "Test Anton", email: "anton@example.com")
+    @post3 = create(:post, content: "Post#3", user: @anton)
   end
 
-  it "displays user posts" do
+  it "displays user own stats and posts" do
     login_as(@user)
     visit user_path(@user)
+    #display stats
     expect(page).to have_selector(".user-avatar")
     expect(page).to have_text("Test User")
     expect(page).to have_text("Email: #{@user.email}")
-    expect(page).to have_text("About: #{@user.about}")
+    expect(page).to have_text("#{@user.about}")
+    #display post form
     expect(page).to have_text("Posts count: #{@user.posts.count}")
+    #display posts
+    expect(page).to have_selector(".post-form")
     expect(page).to have_selector(".posts [id ^= 'post']", count: 2)
     expect(page).to have_text(@post1.content)
     expect(page).to have_text(@post2.content)
+  end
+
+  it "displays other user stats and posts" do
+    login_as(@user)
+    visit user_path(@anton)
+    #display stats
+    expect(page).to have_selector(".user-avatar")
+    expect(page).to have_text("Test Anton")
+    expect(page).to have_text("Email: #{@anton.email}")
+    expect(page).to have_text("#{@anton.about}")
+    expect(page).to have_text("Posts count: #{@anton.posts.count}")
+    #do not display form
+    expect(page).not_to have_selector(".post-form")
+    #display posts
+    expect(page).to have_selector(".posts [id ^= 'post']", count: 1)
+    expect(page).to have_text(@post3.content)
+    #display follow button
+    expect(page).to have_button("Follow")
+  end
+
+  it "displays user followings and followers" do
+    @user.follow(@anton)
+    login_as(@user)
+    visit user_path(@user)
+    expect(page).to have_text("1 following")
+    expect(page).to have_text("0 followers")
+    visit user_path(@anton)
+    expect(page).to have_text("0 following")
+    expect(page).to have_text("1 followers")
+    expect(page).to have_button("Unfollow")
   end
 end
